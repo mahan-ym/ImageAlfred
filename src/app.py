a5933769b376110db0b99a438b817706198ab72b
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2025-06-04 11:39:39 +0200

    init project

---
+++ src/app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+
+def process_image(input_img):
+    pass
+
+
+demo = gr.Interface(
+    fn=process_image,
+    inputs=gr.Image(type="numpy"),
+    outputs=gr.Image(),
+    title="Simple Image Processor",
+    description="Upload an image and see the processed result.",
+
+)
+
+if __name__ == "__main__":
+    demo.launch()
