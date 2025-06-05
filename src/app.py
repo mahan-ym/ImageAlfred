2535835cec3b046240d305eba8a054e7f88fd9fd
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2025-06-05 22:09:46 +0200

    added recolor and privacy_preserve tools for the mcp

--- src/app.py
+++ src/app.py
@@ -1,17 +1,50 @@
 import gradio as gr
 
 
-def process_image(input_img):
+def recolor_image(input_prompt,input_img, target_color):
+    """
+    Recolor an image based on a user-provided prompt and a target color.
+
+    Args:
+        input_prompt (str): The prompt describing the object to recolor.
+        target_color (str): The target color to apply to the object.
+        input_img (numpy.ndarray): The input image as a numpy array.
+
+    Returns:
+        numpy.ndarray: The recolored image.
+    """
     pass
 
+def privacy_preserve_image(input_prompt, input_img):
+    pass
 
-demo = gr.Interface(
-    fn=process_image,
-    inputs=gr.Image(type="numpy"),
+recolor_tool = gr.Interface(
+    fn=recolor_image,
+    inputs=[gr.Textbox("user_input"),gr.Image(type="numpy"),gr.Textbox("target_color")],
     outputs=gr.Image(),
-    title="Simple Image Processor",
-    description="Upload an image and see the processed result.",
+    title="Image Recolor tool",
+    description="Upload an image, provide a prompt for the object to recolor, and specify the target color. The tool will recolor the specified object in the image.",
+)
+
+privacy_preserve_tool = gr.Interface(
+    fn=privacy_preserve_image,
+    inputs=[gr.Textbox("user_input"),gr.Image(type="numpy")],
+    outputs=gr.Image(),
+    title="Privacy preserving tool",
+    description="Upload an image and provide a prompt for the object to enforce privacy. The tool will use blurring to obscure the specified objects in the image.",
+)
+
+demo = gr.TabbedInterface(
+    [recolor_tool, privacy_preserve_tool],
+    ["Recolor", "Privacy preserve"],
+    title="Image Alfred",
+    theme=gr.themes.Default(
+        primary_hue="blue",
+        secondary_hue="blue",
+        font="Inter",
+        font_mono="Courier New",
+    )
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(mcp_server=True)
